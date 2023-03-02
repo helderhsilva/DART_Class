e97231fc17cdb9f404caeee9d9fb88e35e7f545a
theHprogrammer <helder.h.silva@grad.ufsc.br>
2023-03-02 10:19:49 -0300

    Add Conceitos Básicos de Programação

---
+++ Startto_dev_curse/DART_001/while_loop/bin/while_loop.dart
@@ -0,0 +1,15 @@
+void main() {
+  // Estruturas de repetição para estruturar códigos que devem ser repetidos várias vezes.
+
+  int i = 100;
+  int j = 75;
+
+  while (i >= 0 || j > -200) {
+    print(i);
+    print(j);
+    i--;
+    j -= 10;
+  }
+
+  print('Repetição terminada.');
+}
